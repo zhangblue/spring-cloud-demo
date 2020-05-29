4ef76bcebb8950c1e7a69880e4ed7b5ed70062ca
zhangblue <zhangdi013@sina.com>
2020-05-30 00:53:22 +0800

    add server discovery

--- cloud-provider-payment/src/main/java/com/zhangd/springcloud/PaymentMain.java
+++ cloud-provider-payment/src/main/java/com/zhangd/springcloud/PaymentMain.java
@@ -3,6 +3,7 @@ package com.zhangd.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class PaymentMain {
 
   public static void main(String[] args) {
--- cloud-provider-payment/src/main/java/com/zhangd/springcloud/controller/PaymentController.java
+++ cloud-provider-payment/src/main/java/com/zhangd/springcloud/controller/PaymentController.java
@@ -1,10 +1,15 @@
 package com.zhangd.springcloud.controller;
 
+
+import com.alibaba.fastjson.JSONObject;
 import com.zhangd.springcloud.entities.CommonResult;
 import com.zhangd.springcloud.entities.Payment;
 import com.zhangd.springcloud.service.PaymentService;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,8 +27,11 @@ public class PaymentController {
   @Autowired
   private PaymentService paymentService;
 
+  @Autowired
+  private DiscoveryClient discoveryClient;
 
-  @RequestMapping(value = "/create",method = RequestMethod.POST)
+
+  @RequestMapping(value = "/create", method = RequestMethod.POST)
   public CommonResult<Payment> create(@RequestBody Payment payment) {
 
     log.info(payment.getSerial());
@@ -45,5 +53,32 @@ public class PaymentController {
     } else {
       return new CommonResult<Payment>(444, "get error");
     }
+  }
+
+  /**
+   * 服务发现
+   *
+   * @return
+   */
+  @RequestMapping(value = "/discovery", method = RequestMethod.GET)
+  public Object discovery() {
+
+    /**
+     *
+     */
+    List<String> services = discoveryClient.getServices();
+    for (String service : services) {
+      log.info("Application = [{}]", service);
+    }
+
+    /**
+     * 根据实例id获取所有服务名称
+     */
+    List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+    for (ServiceInstance instance : instances) {
+      log.info("Instance = [{}]", JSONObject.toJSONString(instance));
+    }
+
+    return this.discoveryClient;
   }
 }
